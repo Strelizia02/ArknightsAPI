eb1d8ec3348b3490dbc83c41ba01f94d863e2848
WANGZYAAH <WANGZYAAH@digitalchina.com>
2020-12-15 18:36:49 +0800

    公招tag补全

--- src/main/java/com/strelizia/arknights/util/TagsUtil.java
+++ src/main/java/com/strelizia/arknights/util/TagsUtil.java
@@ -36,18 +36,4 @@ public class TagsUtil {
         return result;
     }
 
-    public static void main(String[] args) {
-        String a = "远程位,治疗,支援机械,医疗,近战位,支援,支援机械,近卫,近战位,新手,先锋,远程位,新手,术师,远程位,新手,术师,远程位,新手,狙击,近战位,新手,重装,近战位,费用回复,先锋,近战位,费用回复,先锋,近战位,输出,费用回复,先锋,远程位,治疗,医疗,远程位,治疗,医疗,远程位,群攻,术师,远程位,输出,术师,远程位,输出,狙击,远程位,输出,狙击,远程位,减速,辅助,近战位,输出,生存,近卫,近战位,防护,重装,近战位,费用回复,输出,先锋,近战位,输出,费用回复,先锋,远程位,治疗,医疗,远程位,治疗,医疗,削弱,远程位,输出,术师,远程位,群攻,术师,近战位,快速复活,防护,特种,近战位,位移,特种,近战位,位移,特种,远程位,群攻,减速,狙击,远程位,输出,削弱,狙击,远程位,输出,生存,狙击,远程位,减速,辅助,近战位,支援,输出,近卫,近战位,群攻,生存,近卫,近战位,输出,近卫,近战位,减速,输出,近卫,输出,近战";
-        String[] split = a.split(",");
-        List<String> strings = Arrays.asList(split);
-        List<String> newList = new ArrayList<>();
-        Set set = new HashSet();
-        for (String s:strings){
-            if (set.add(s)){
-                newList.add(s);
-            }
-        }
-        System.out.println(newList);
-    }
-
 }
