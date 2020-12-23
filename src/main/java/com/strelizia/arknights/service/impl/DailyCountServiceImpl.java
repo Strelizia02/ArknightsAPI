e5ec4dfa07874cbb61f111aee5ea1ef7c24302e3
WANGZYAAH <WANGZYAAH@digitalchina.com>
2020-12-23 09:33:11 +0800

    取消日报配图

--- src/main/java/com/strelizia/arknights/service/impl/DailyCountServiceImpl.java
+++ src/main/java/com/strelizia/arknights/service/impl/DailyCountServiceImpl.java
@@ -34,13 +34,13 @@ public class DailyCountServiceImpl implements DailyCountService {
         UserGetInfo foundCount = userFoundMapper.selectFoundCountMax(groupId);
         UserGetInfo todayFeiQiu = userFoundMapper.selectRateTodayMin(groupId);
         Integer usersCount = userFoundMapper.foundNoSixToday(groupId, todayCount);
-        String s = "截止今晚8:00，本群内";
-        if (allRateMax != null)s = s + "模拟寻访累计出货最多的欧皇为：" + allSixMax.getName() + "累计获得"+ allSixMax.getValue() + "个六星\n";
-        if (allRateMax != null)s = s + "模拟寻访累计爆率最高的欧皇为：" + allRateMax.getName() + "六星概率为："+ allRateMax.getValue() + "%\n";
-        if (todaySixMax != null)s = s + "今日获取六星最多的是：" + todaySixMax.getName() + "共抽取了" + todaySixMax.getValue() + "个六星\n";
-        if (foundCount != null)s = s +  "目前垫刀数最多的人为：" + foundCount.getName() + "垫刀数为：" + foundCount.getValue() + "加油，也许下一发就出货了\n";
-        s = s +"今日群内共有" + usersCount + "个群友机会用完还没六星，现在白给这些群友十抽，抓紧时间，这可能就是去往欧洲的船票\n";
-        if (todayFeiQiu != null)s = s + "今日非酋为：" + todayFeiQiu.getName() + "五星六星爆率合计竟高达" + todayFeiQiu.getValue() + "%人与人的欧气果然不能相提并论\n";
+        String s = "截止今晚8:00，本群内:\n";
+        if (allRateMax != null)s = s + "模拟寻访累计出货最多的欧皇为：" + allSixMax.getName() + "累计获得"+ allSixMax.getValue() + "个六星\n\n";
+        if (allRateMax != null)s = s + "模拟寻访累计爆率最高的欧皇为：" + allRateMax.getName() + "六星概率为："+ allRateMax.getValue() + "%\n\n";
+        if (todaySixMax != null)s = s + "今日获取六星最多的是：" + todaySixMax.getName() + "共抽取了" + todaySixMax.getValue() + "个六星\n\n";
+        if (foundCount != null)s = s +  "目前垫刀数最多的人为：" + foundCount.getName() + "垫刀数为：" + foundCount.getValue() + "加油，也许下一发就出货了\n\n";
+        s = s +"今日群内共有" + usersCount + "个群友机会用完还没六星，现在白给这些群友十抽，抓紧时间，这可能就是去往欧洲的船票\n\n";
+        if (todayFeiQiu != null)s = s + "今日非酋为：" + todayFeiQiu.getName() + "五星六星爆率合计竟高达" + todayFeiQiu.getValue() + "%人与人的欧气果然不能相提并论\n\n";
         if (todayRateMax != null)s = s + "今日欧皇为：" + todayRateMax.getName() + "六星爆率为" + todayRateMax.getValue() + "%让我们恭喜这个b";
         return s;
     }
@@ -50,8 +50,8 @@ public class DailyCountServiceImpl implements DailyCountService {
         List<Long> groups = userFoundMapper.selectAllActiveGroups();
         for (Long groupId:groups){
             String s = jobMessageByGroupId(groupId);
-            String imgUrl = "http://gchat.qpic.cn/gchatpic_new/412459523/901158551-2534335053-0917C33A201AC78760663890D4073968/0?vuin=3022645754\\u0026term=255\\u0026pictype=0";
-            sendMsgUtil.CallOPQApiSendImg(groupId,s,imgUrl);
+//            String imgUrl = "http://gchat.qpic.cn/gchatpic_new/412459523/901158551-2534335053-0917C33A201AC78760663890D4073968/0?vuin=3022645754\\u0026term=255\\u0026pictype=0";
+            sendMsgUtil.CallOPQApiSendMsg(groupId,s);
         }
         userFoundMapper.giveMoreFoundToFeiQiu(todayCount);
     }
