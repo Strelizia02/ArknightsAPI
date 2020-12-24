feed3425f9d0131c302019242666a80310dd733a
WANGZYAAH <WANGZYAAH@digitalchina.com>
2020-12-24 17:19:37 +0800

    日志优化

--- src/main/java/com/strelizia/arknights/util/SendMsgUtil.java
+++ src/main/java/com/strelizia/arknights/util/SendMsgUtil.java
@@ -97,7 +97,7 @@ public class SendMsgUtil {
     public void CallOPQApiSendImg(Long groupId, String s, String imgUrl ,Integer sendToType){
         poolTaskExecutor.execute(() -> sendTextImgToGroup(restTemplate, groupId, s, imgUrl,
                 "http://" + OPQUrl + ":8888" + sendTextMsgApi + "?qq=" +  loginQq + "&funcname=SendMsg",sendToType));
-        log.info("发送消息图片+文字{}成功",s);
+        log.info("发送消息图片{}+文字{}成功",imgUrl,s);
     }
 
 }
