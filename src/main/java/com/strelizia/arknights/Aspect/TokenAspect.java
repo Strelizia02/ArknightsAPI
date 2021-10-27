d54be3a8e12c042822c85f91fe8f5a4538fd416d
wangzyaah <wangzyaah@digitalchina.com>
2021-10-27 18:02:46 +0800

    解决AOP无返回值情况

--- src/main/java/com/strelizia/arknights/Aspect/TokenAspect.java
+++ src/main/java/com/strelizia/arknights/Aspect/TokenAspect.java
@@ -6,13 +6,8 @@ import com.strelizia.arknights.vo.JsonResult;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -38,13 +33,8 @@ public class TokenAspect {
 
     }
 
-    @Before("annotationPointcut()")
-    public void beforePointcut(JoinPoint joinPoint) {
-        // 此处进入到方法前  可以实现一些业务逻辑
-    }
-
     @Around("annotationPointcut()")
-    public void doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
         String token = request.getHeader(TOKEN_KEY);
         String mes;
         if (null == token){
@@ -53,21 +43,14 @@ public class TokenAspect {
             Boolean isTrue = this.checkToken(token);
             if (isTrue) {
                 loginMapper.refreshToken(token);
-                joinPoint.proceed();
-                return;
+                return joinPoint.proceed();
             } else {
                 mes = "token is not alive";
             }
         }
         response.setHeader("Content-Type", "application/json");
         response.getWriter().write(new ObjectMapper().writeValueAsString(JsonResult.failureWithCode("301", mes)));
-    }
-
-    /**
-     * 在切入点return内容之后切入内容（可以用来对处理返回值做一些加工处理）
-     */
-    @AfterReturning("annotationPointcut()")
-    public void doAfterReturning(JoinPoint joinPoint) {
+        return null;
     }
 
     private Boolean checkToken(String token) {
